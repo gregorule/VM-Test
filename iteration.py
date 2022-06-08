33699926a3877054b587483c8180938f99057d98
gregorule <gregorule8@gmail.com>
2022-06-08 11:21:36 +0000

    iteration commit

---
+++ iteration.py
@@ -0,0 +1,29 @@
+'''
+count = 0
+name = str(input("What is your name?"))
+
+while count < 5:
+    print(name, "is awesome!")
+    count += 1
+
+for i in range(5, 11):
+    print(i)
+'''
+'''
+count = 0
+name1 = str(input("What is the name of the 1st person?"))
+name2 = str(input("What is the name of the 2nd person?"))
+name3 = str(input("What is the name of the 3rd person?"))
+name4 = str(input("What is the name of the 4th person?"))
+name5 = str(input("What is the name of the 5th person?"))
+
+while count < 1:
+    print(name1, "is awesome!")
+    print(name2, "is awesome!")
+    print(name3, "is awesome!")
+    print(name4, "is awesome!")
+    print(name5, "is awesome!")
+    count += 1
+'''
+for i in range(10, 21, 2):
+    print(i)
